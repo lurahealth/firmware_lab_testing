8b129bcbac948cdd940328498a08dd42d18b7f1e
uchu-biosensors <noah@uchubiosensors.com>
2020-01-21 18:59:00 +0800

    first commit

---
+++ pca10040e/s112/ses/Output/ble_app_uart_pca10040e_s112 Release/Obj/SEGGER_RTT_Syscalls_SES.d
@@ -0,0 +1,3 @@
+Output/ble_app_uart_pca10040e_s112\ Release/Obj/SEGGER_RTT_Syscalls_SES.o: \
+ /home/noah/programming/firmware/nRF5_SDK_15.2.0_9412b96/external/segger_rtt/SEGGER_RTT_Syscalls_SES.c \
+ ../config/sdk_config.h
---
+++ pca10040e/s112/ses/Output/ble_app_uart_pca10040e_s112 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_uart_pca10040e_s112\ Release/Obj/utf.o: \
+ /home/noah/programming/firmware/nRF5_SDK_15.2.0_9412b96_STABLE/external/utf_converter/utf.c \
+ /home/noah/programming/firmware/nRF5_SDK_15.2.0_9412b96_STABLE/external/utf_converter/utf.h \
+ /usr/share/segger_embedded_studio_for_arm_4.22/include/stddef.h \
+ /usr/share/segger_embedded_studio_for_arm_4.22/include/__crossworks.h \
+ /usr/share/segger_embedded_studio_for_arm_4.22/include/stdint.h
