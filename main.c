ec78f311c5f81af41b039aec74deb3ae3a9a05b8
lurahealth <noah@lurahealth.com>
2020-09-17 14:47:53 +0800

    send cal results packet (M, B, R^2), more detailed PTx_CONF response

--- main.c
+++ main.c
@@ -164,6 +164,13 @@
     .pin_n      = NRF_SAADC_INPUT_DISABLED          \
 }
 
+#define PACKET_FLOAT_MARKER "%s%d.%1d"
+#define PACKET_RVAL_MARKER "%s%d.%3d"
+#define LOG_PACKET_FLOAT(val, dec) (uint32_t)(((val) < 0 && (val) > -1.0) ? "-" : ""),   \
+                             (int32_t)(val),                                       \
+                             (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
+                                                    : (int32_t)(val) - (val))*pow(10,dec))
+
 /* UNDEFS FOR DEBUGGING */
 #undef RX_PIN_NUMBER
 #undef RTS_PIN_NUMBER
@@ -201,13 +208,14 @@ bool     CAL_MODE          = false;
 bool     PT1_READ          = false;
 bool     PT2_READ          = false;
 bool     PT3_READ          = false;
-float   PT1_PH_VAL        = 0;
-float   PT1_MV_VAL        = 0;
-float   PT2_PH_VAL        = 0;
-float   PT2_MV_VAL        = 0;
-float   PT3_PH_VAL        = 0;
-float   PT3_MV_VAL        = 0;
-float   REF_TEMP          = 0;
+float     PT1_PH_VAL        = 0;
+float     PT1_MV_VAL        = 0;
+float     PT2_PH_VAL        = 0;
+float     PT2_MV_VAL        = 0;
+float     PT3_PH_VAL        = 0;
+float     PT3_MV_VAL        = 0;
+float     REF_TEMP          = 0;
+float     CURR_TEMP         = 0;
 int      NUM_CAL_PTS       = 0;
 float     MVAL_CALIBRATION  = 0;
 float     BVAL_CALIBRATION  = 0;
@@ -239,8 +247,9 @@ void restart_pH_interval_timer  (void);
 void write_cal_values_to_flash   (void);
 void linreg                     (int num, float x[], float y[]);
 void perform_calibration        (uint8_t cal_pts);
-float calculate_pH_from_mV     (uint32_t ph_val);
-float calculate_celsius_from_mv(uint32_t mv);
+float calculate_pH_from_mV       (uint32_t ph_val);
+float calculate_celsius_from_mv  (uint32_t mv);
+float validate_float_range        (float val);
 static void advertising_start   (bool erase_bonds);
 uint32_t saadc_result_to_mv     (uint32_t saadc_result);
 
@@ -515,18 +524,25 @@ void read_saadc_and_set_ref_temp(int samples)
     }
     AVG_MV_VAL = AVG_MV_VAL / samples;
     if (!PT1_READ) {
-        REF_TEMP = calculate_celsius_from_mv(AVG_MV_VAL);
-        PT1_READ = true;
+        CURR_TEMP = calculate_celsius_from_mv(AVG_MV_VAL);
+        CURR_TEMP = validate_float_range(CURR_TEMP); 
+        NRF_LOG_INFO("CURR_TEMP: " NRF_LOG_FLOAT_MARKER " \n", NRF_LOG_FLOAT(CURR_TEMP));
+        REF_TEMP  = CURR_TEMP;
+        PT1_READ  = true;
     }
     else if (PT1_READ && !PT2_READ) {
-        REF_TEMP = REF_TEMP + calculate_celsius_from_mv(AVG_MV_VAL);
+        CURR_TEMP = calculate_celsius_from_mv(AVG_MV_VAL);
+        CURR_TEMP = validate_float_range(CURR_TEMP); 
+        REF_TEMP = REF_TEMP + CURR_TEMP;
         if (NUM_CAL_PTS == 2) {REF_TEMP = REF_TEMP / 2.0;}
         PT2_READ = true;
     }
     else if (PT1_READ && PT2_READ && !PT3_READ) {
-       REF_TEMP = REF_TEMP + calculate_celsius_from_mv(AVG_MV_VAL);
-       REF_TEMP = REF_TEMP / 3.0;
-       PT3_READ = true;
+        CURR_TEMP = calculate_celsius_from_mv(AVG_MV_VAL);
+        CURR_TEMP = validate_float_range(CURR_TEMP); 
+        REF_TEMP = REF_TEMP + CURR_TEMP;
+        REF_TEMP = REF_TEMP / 3.0;
+        PT3_READ = true;
     }
     NRF_LOG_INFO("ref temp: " NRF_LOG_FLOAT_MARKER " ", NRF_LOG_FLOAT(REF_TEMP));
 }
@@ -596,6 +612,47 @@ void perform_calibration(uint8_t cal_pts)
   }
 }
 
+/* Packs the mV value and temperature reading recorded for the current
+ * calibration point into the confirmation packet
+ */
+void pack_cal_values_into_confirm_packet(char confirm_packet[3][24], int cal_pt) {
+      // Pack packet depending on calibration point
+      if (cal_pt == 1)
+          sprintf(confirm_packet[cal_pt-1], "PT1CONF %u mV, " PACKET_FLOAT_MARKER " C\n", 
+                                           (uint32_t)PT1_MV_VAL, LOG_PACKET_FLOAT(CURR_TEMP,1));
+      else if (cal_pt == 2)
+          sprintf(confirm_packet[cal_pt-1], "PT2CONF %u mV, " PACKET_FLOAT_MARKER " C\n", 
+                                           (uint32_t)PT2_MV_VAL, LOG_PACKET_FLOAT(CURR_TEMP,1));
+      else if (cal_pt == 3)
+          sprintf(confirm_packet[cal_pt-1], "PT3CONF %u mV, " PACKET_FLOAT_MARKER " C\n", 
+                                           (uint32_t)PT3_MV_VAL, LOG_PACKET_FLOAT(CURR_TEMP,1));
+}
+
+/* Packs the results of calibration into a packet, including M and B values from
+ * Y = Mx + B, the R^2 accuracy of the linear regression, and the reference temp.
+ *
+ * When converting mV to analyte values, this application uses an calibration 
+ * with units such that M is "pH / mV" and B is pH. This packing function coverts 
+ * M to "mV / pH" and B to mV, as these are the most useful values to the user
+ */
+void pack_lin_reg_values_into_packet(char report_packet[45], uint16_t *pack_len)
+{
+    uint16_t len = 0;
+    for (int i = 0; i < 45; i++) {report_packet[i] = 0;}
+    sprintf(report_packet, "M=" PACKET_FLOAT_MARKER ", B=" PACKET_FLOAT_MARKER ", "
+                           "R=" PACKET_RVAL_MARKER ", C=" PACKET_FLOAT_MARKER " \n", 
+                                    LOG_PACKET_FLOAT(1.0/MVAL_CALIBRATION,1),        
+                                    LOG_PACKET_FLOAT(BVAL_CALIBRATION/MVAL_CALIBRATION*-1,1),   
+                                    LOG_PACKET_FLOAT(fabs(RVAL_CALIBRATION),3),      
+                                    LOG_PACKET_FLOAT(REF_TEMP,1));
+   for (int i = 0; i < 45; i++) {
+      if (report_packet[i] > 0)
+        len++;
+   }
+   report_packet[len] = '\n';
+   *pack_len = len+2;
+}
+
 /*
  * Checks packet contents to appropriately perform calibration
  */
@@ -606,11 +663,13 @@ void check_for_calibration(char **packet)
     char *PWROFF    = "PWROFF";
     char *PT        = "PT";
     // Possible strings to send to mobile application
-    char *CALBEGIN = "CALBEGIN";
-    char PT_CONFS[3][8] = {"PT1CONF", "PT2CONF", "PT3CONF"};
+    char CALBEGIN[9] = {"CALBEGIN\n"};   
+    char CALRESULTS[45];
+    char PT_CONFS[3][24];
     // Variables to hold sizes of strings for ble_nus_send function
-    uint16_t SIZE_BEGIN = 9;
-    uint16_t SIZE_CONF  = 8;
+    uint16_t SIZE_BEGIN   = 9;
+    uint16_t SIZE_CONF    = 24;
+    uint16_t SIZE_RESULTS;
     // Used for parsing out pH value from PT1_X.Y (etc) packets
     char pH_val_substring[4];
 
@@ -649,12 +708,20 @@ void check_for_calibration(char **packet)
             PT3_PH_VAL = atof(pH_val_substring); 
         // Read calibration data and send confirmation packet
         read_saadc_for_calibration();
+        pack_cal_values_into_confirm_packet(PT_CONFS, cal_pt);
         err_code = ble_nus_data_send(&m_nus, PT_CONFS[cal_pt - 1], 
                                      &SIZE_CONF, m_conn_handle);
         APP_ERROR_CHECK(err_code);
         // Restart normal data transmission if calibration is complete
         if (NUM_CAL_PTS == cal_pt) {
           perform_calibration(cal_pt);
+          pack_lin_reg_values_into_packet(CALRESULTS, &SIZE_RESULTS);
+          NRF_LOG_INFO("length: %u", SIZE_RESULTS);
+          for(int i = 0; i < SIZE_RESULTS; i++) {
+              NRF_LOG_INFO("%c", CALRESULTS[i]);
+          }
+          err_code = ble_nus_data_send(&m_nus, CALRESULTS, &SIZE_RESULTS, m_conn_handle);
+          APP_ERROR_CHECK(err_code);
           write_cal_values_to_flash();
           reset_calibration_state();
           restart_pH_interval_timer();
